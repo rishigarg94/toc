956a83ce9268ad3b94062e31267174ad5c73225e
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2021-04-11 06:48:26 +0530

    fixed speechly

--- client/src/components/blockchain/Dashboard.js
+++ client/src/components/blockchain/Dashboard.js
@@ -70,7 +70,7 @@ const Dashboard = () => {
     const handleLogout = async () => {
         // portis.logout()
         let accounts = await web3.eth.getAccounts();
-        setAccount(accounts[4])
+        setAccount(accounts[0])
         // console.log("recent account is ",account);
 
         // await hack.methods.get_msg_sender().call((err,res) =>{
